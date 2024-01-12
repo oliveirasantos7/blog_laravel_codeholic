dfa460a0aea5b2afaff6aecb242152624dc19560
oliveira santos <santosoliveira482@gmail.com>
2024-01-12 06:39:49 -0300

    criação das paginas de post

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ use App\Http\Controllers\PostController;
 Route::get('/',[PostController::class, 'index'])->name('home');
 Route::get('/{post:slug}',[PostController::class, 'show'])->name('view');
 
+
